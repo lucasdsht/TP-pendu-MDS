e8a5f014af8d68418b1abb354305974e0ca15f9a
lucasdsht <lucasdouchet17112003@gmail.com>
2024-01-31 14:37:58 +0100

    base layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { Routes } from 'react-router-dom'
 import './App.css'
 import { Route } from 'react-router-dom'
 import { Home } from './pages'
+import { Layout } from './components/Layout'
 
 function App() {
 
   return (
-    <Routes>
-      <Route path='*' element={<Home />} />
-      <Route path='/' element={<Home />} />
-      <Route path='/play' element={<Home />} />
-      <Route path='/stats' element={<Home />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path='*' element={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/play' element={<Home />} />
+        <Route path='/stats' element={<Home />} />
+      </Routes>
+    </Layout>
   )
 }
 
