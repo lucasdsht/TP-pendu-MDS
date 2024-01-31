8406cd09e6295171adf1ed3c6987ccf886bb4fd4
Dimitri <dimitri@livin-france.com>
2024-01-31 14:34:00 +0100

    newComponentNav

---
+++ src/components/nav.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Accueil</Link>
+        </li>
+        <li>
+          <Link to="/about">À propos</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
