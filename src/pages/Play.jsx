d2a04d6adfcc5fe6ef79c3767ccfd3abe045dd19
Dimitri <dimitri@livin-france.com>
2024-02-21 08:55:36 +0100

    Add_win_input

--- src/pages/Play.jsx
+++ src/pages/Play.jsx
@@ -9,10 +9,10 @@ export const Play = () => {
   const [selectedLetters, setSelectedLetters] = useState([]);
   const [guess, setGuess] = useState('');
   const [wrongGuesses, setWrongGuesses] = useState([]);
+  const [name, setName] = useState(''); // Ajout d'une variable d'état pour le nom
   const { word, loading, error } = useWord();
   const { win, setWin, loose, setLoose, trials, setTrials } = useStats();
   const [disable, setDisable] = useState(false);
-
 
   const loooseGame = () => {
     if (trials == 9) {
@@ -25,8 +25,15 @@ export const Play = () => {
     console.log(selectedWord);
     console.log(word.toUpperCase().split('').every(letter => selectedWord.includes(letter)), "|||")
     if (word.toUpperCase().split('').every(letter => selectedWord.includes(letter))) {
+      if(!win){
       setWin(true);
       alert("Congratulations! You've guessed the word.");
+    }
+    const scores = JSON.parse(localStorage.getItem('scores')) || [];
+
+    scores.push({ name, trials });
+  
+    localStorage.setItem('scores', JSON.stringify(scores));
     }
   }
 
@@ -71,17 +78,34 @@ export const Play = () => {
     }
   };
 
+  const handleNameSubmit = (e) => {
+    e.preventDefault();
+
+    const scores = JSON.parse(localStorage.getItem('scores')) || [];
+
+    scores.push({ name, trials });
+
+    localStorage.setItem('scores', JSON.stringify(scores));
+
+    setName('');
+  }
+
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
   console.log(word);
-
 
   return (
     <>
       <h1>Jouer au pendu</h1>
       <WordInput word={word} selectedLetters={selectedLetters} />
       <LetterChoice disable={disable} selectedLetters={selectedLetters} onLetterSelect={addSelectedLetter} />
+      {win && (
+               <form onSubmit={handleNameSubmit}>
+               <input type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="Votre nom" />
+               <button type="submit">Envoyer</button>
+             </form>
+      )}
       <div>
         <form onSubmit={handleGuessSubmit}>
           <label htmlFor="word-guess">Trouvez le mot</label>
@@ -101,4 +125,3 @@ export const Play = () => {
     </>
   );
 };
-
--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,10 +1,16 @@
 export const Stats = () => {
-    const stats = "classement"
-  
-    return (
-      <>
-        <h1>{stats}</h1>
-      </>
-    )
-  }
-  
+  const stats = "classement";
+  const scores = JSON.parse(localStorage.getItem('scores')) || [];
+
+  // Créer une liste de scores pour l'affichage
+  const scoreList = scores.map((score, index) => (
+    <p key={index}>Name: {score.name}, Trials: {score.trials}</p>
+  ));
+
+  return (
+    <>
+      <h1>{stats}</h1>
+      {scoreList}
+    </>
+  );
+}
